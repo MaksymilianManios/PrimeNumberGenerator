fbe65aa00311f847ccfed729a9fe9d3126664b07
maksymilian manios <makman1998@gmail.com>
2020-07-12 16:47:28 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrimeNumberGenerator
+{
+    class Program
+    {
+        public static int[] NumbersList;
+        public static int[] Deviders;
+
+        public int[] PrimeNumbersGenerator(int range)
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
